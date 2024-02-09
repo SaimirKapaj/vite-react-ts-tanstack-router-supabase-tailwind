a156cae69b209d2cda77398d62e54885d518942f
SaimirKapaj <kapajsaimir@gmail.com>
2024-02-09 09:46:24 +0100

    changed useAuth to arrow function

--- src/auth.tsx
+++ src/auth.tsx
@@ -85,11 +85,11 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-export function useAuth() {
+export const useAuth = () => {
   const context = useContext(AuthContext);
 
   if (!context) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
   return context;
-}
+};
